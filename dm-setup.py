d46d1cf11e096b5c6db6144549147154bf08b967
Roland Ding <32445200+shengxio@users.noreply.github.com>
2020-09-16 20:56:25 -0600

    Update dm-setup.py

--- dm-setup.py
+++ dm-setup.py
@@ -1,23 +1,28 @@
 # this main script is to create a genenral run file for datamind runtime environment
 # following code block will import all contents for the programming.
-try:
-  import sklearn
-  import pandas as pd
-  import numpy as np
-  import sweetviz as sv
-  import seaborn as sns
-except ImportError:
-  userChoice = input("Found missing package, auto install? (yes/no):\n")
-  if userChoice.lower()=="yes":
-    ! pip install sklearn
-    ! pip install pandas
-    ! pip install numpy
-    ! pip install sweetviz
-    ! pip install seaborn
-   elif userChoice.lower() =="no":
-    print("exiting Datamind initial setup....")
-   else:
-    print("don't know what I have read. terminating.....BANG!")
-except Exception as error:
-  print(error,False)
-  print(error.__class__.__name__ + ": " + error.message)
+def dm_setup():
+  try:
+    import sklearn
+    import pandas as pd
+    import numpy as np
+    import sweetviz as sv
+    import seaborn as sns
+  except ImportError:
+    userChoice = input("Found missing package, auto install? (yes/no):\n")
+    if userChoice.lower()=="yes":
+      ! pip install sklearn
+      ! pip install pandas
+      ! pip install numpy
+      ! pip install sweetviz
+      ! pip install seaborn
+     elif userChoice.lower() =="no":
+      print("exiting Datamind initial setup....")
+     else:
+      print("don't know what I have read. terminating.....BANG!")
+  except Exception as error:
+    print(error,False)
+    print(error.__class__.__name__ + ": " + error.message)
+
+if __name__=="__main__":
+  dm_setup()
+    
